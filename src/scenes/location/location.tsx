6215080bd79a897428904fb3d8275c72fd334a77
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2024-01-24 23:50:39 +0100

    fix: BottomSheetScrollView

--- src/scenes/location/location.tsx
+++ src/scenes/location/location.tsx
@@ -13,13 +13,14 @@ import {
   TextInput,
   type TextInputChangeEventData,
   View,
+  ScrollView,
 } from 'react-native';
 import MyText from '~/components/ui/my-text';
 import * as Location from 'expo-location';
 import { useAddress } from '~/zustand/address/useAddress';
 import { type Feature, type Address } from '~/types/location';
 import { LocationService } from '~/services/location';
-import BottomSheet from '@gorhom/bottom-sheet';
+import BottomSheet, { BottomSheetScrollView } from '@gorhom/bottom-sheet';
 import { useNavigation } from '@react-navigation/native';
 import { LocationIcon } from '~/assets/icons/location';
 import { cn } from '~/utils/tailwind';
@@ -176,130 +177,132 @@ export function LocationPage(props: LocationPageProps) {
         </View>
       </View>
 
-      <View className="flex items-start justify-start bg-app-gray">
-        <View className="w-full  p-4">
-          <Pressable
-            onPress={async () => {
-              logEvent({
-                category: 'LOCATION',
-                action: 'SELECT_GEOLOCATION',
-              });
-              Location.requestForegroundPermissionsAsync().then(
-                async ({ status }) => {
-                  if (status !== 'granted') {
-                    Alert.alert(
-                      'Permission not granted to access your location',
-                      'You can change that in your settings',
-                      [
-                        {
-                          text: 'Open Settings',
-                          onPress: async () => {
-                            await Linking.openSettings();
+      <BottomSheetScrollView className="flex flex-1 bg-app-gray">
+        <View className="px-6 pt-6">
+          <View className="w-full  p-4">
+            <Pressable
+              onPress={async () => {
+                logEvent({
+                  category: 'LOCATION',
+                  action: 'SELECT_GEOLOCATION',
+                });
+                Location.requestForegroundPermissionsAsync().then(
+                  async ({ status }) => {
+                    if (status !== 'granted') {
+                      Alert.alert(
+                        'Permission not granted to access your location',
+                        'You can change that in your settings',
+                        [
+                          {
+                            text: 'Open Settings',
+                            onPress: async () => {
+                              await Linking.openSettings();
+                            },
                           },
-                        },
-                        {
-                          text: 'OK',
-                          style: 'cancel',
-                          onPress: () => {
-                            navigation.goBack();
+                          {
+                            text: 'OK',
+                            style: 'cancel',
+                            onPress: () => {
+                              navigation.goBack();
+                            },
                           },
-                        },
-                      ],
-                    );
-                    return;
-                  }
-                  const location = await Location.getCurrentPositionAsync({});
-                  const { latitude, longitude } = location.coords;
+                        ],
+                      );
+                      return;
+                    }
+                    const location = await Location.getCurrentPositionAsync({});
+                    const { latitude, longitude } = location.coords;
 
-                  const formatedAdress =
-                    await LocationService.getAdressByCoordinates(
-                      latitude,
-                      longitude,
-                    );
-                  if (formatedAdress) {
-                    handleSelect(formatedAdress);
-                  }
-                },
-              );
-            }}
-            className="flex flex-row items-center justify-start "
-          >
-            <LocationIcon color="black" />
+                    const formatedAdress =
+                      await LocationService.getAdressByCoordinates(
+                        latitude,
+                        longitude,
+                      );
+                    if (formatedAdress) {
+                      handleSelect(formatedAdress);
+                    }
+                  },
+                );
+              }}
+              className="flex flex-row items-center justify-start "
+            >
+              <LocationIcon color="black" />
 
-            <MyText
-              font="MarianneBold"
-              className="ml-4 w-fit text-[14px] text-black"
-            >
-              Utiliser ma géolocalisation
-            </MyText>
-          </Pressable>
-        </View>
-
-        {status === 'loading' ? (
-          <View className="w-full  border-b  border-gray-300 p-4  ">
-            <MyText
-              font="MarianneRegular"
-              className="ml-4 w-fit text-lg text-black"
-            >
-              Chargement...
-            </MyText>
-          </View>
-        ) : null}
-        {status === 'idle' ? (
-          <View className="mt-16 h-full w-full">
-            <Illu />
-            <MyText
-              font="MarianneMedium"
-              className="mt-4 px-12 text-center text-[14px] text-gray-700"
-            >
-              Optimisez votre expérience en activant la géolocalisation afin
-              d’améliorer votre utilisation de l'application.
-            </MyText>
-          </View>
-        ) : null}
-        {status === 'no_result' ? (
-          <View className="mt-16 h-full w-full">
-            <Illu />
-            <MyText
-              font="MarianneMedium"
-              className="mt-4 px-4 text-center text-[14px] text-gray-700"
-            >
-              Aucun résultat n'a été trouvé pour cette recherche.
-            </MyText>
-          </View>
-        ) : null}
-        {suggestedAddress?.map((address) => {
-          return (
-            <View
-              key={`${address.id}-${address.postcode}$}`}
-              className="w-full  border-b  border-gray-300 p-4  "
-            >
-              <Pressable
-                onPress={() => {
-                  logEvent({
-                    category: 'LOCATION',
-                    action: 'SELECT_ADDRESS',
-                  });
-                  handleSelect(address);
-                }}
+              <MyText
+                font="MarianneBold"
+                className="ml-4 w-fit text-[14px] text-black"
               >
-                <MyText
-                  font="MarianneRegular"
-                  className="mb-2  w-full   text-xl text-black"
-                >
-                  {address.label}
-                </MyText>
-                <MyText
-                  font="MarianneBold"
-                  className="  w-full   text-xl text-black"
-                >
-                  {address.postcode} {address.city}
-                </MyText>
-              </Pressable>
+                Utiliser ma géolocalisation
+              </MyText>
+            </Pressable>
+          </View>
+
+          {status === 'loading' ? (
+            <View className="w-full  border-b  border-gray-300 p-4  ">
+              <MyText
+                font="MarianneRegular"
+                className="ml-4 w-fit text-lg text-black"
+              >
+                Chargement...
+              </MyText>
             </View>
-          );
-        })}
-      </View>
+          ) : null}
+          {status === 'idle' ? (
+            <View className="mt-16 h-full w-full">
+              <Illu />
+              <MyText
+                font="MarianneMedium"
+                className="mt-4 px-12 text-center text-[14px] text-gray-700"
+              >
+                Optimisez votre expérience en activant la géolocalisation afin
+                d’améliorer votre utilisation de l'application.
+              </MyText>
+            </View>
+          ) : null}
+          {status === 'no_result' ? (
+            <View className="mt-16 h-full w-full">
+              <Illu />
+              <MyText
+                font="MarianneMedium"
+                className="mt-4 px-4 text-center text-[14px] text-gray-700"
+              >
+                Aucun résultat n'a été trouvé pour cette recherche.
+              </MyText>
+            </View>
+          ) : null}
+          {suggestedAddress?.map((address) => {
+            return (
+              <View
+                key={`${address.id}-${address.postcode}$}`}
+                className="w-full  border-b  border-gray-300 p-4  "
+              >
+                <Pressable
+                  onPress={() => {
+                    logEvent({
+                      category: 'LOCATION',
+                      action: 'SELECT_ADDRESS',
+                    });
+                    handleSelect(address);
+                  }}
+                >
+                  <MyText
+                    font="MarianneRegular"
+                    className="mb-2  w-full   text-xl text-black"
+                  >
+                    {address.label}
+                  </MyText>
+                  <MyText
+                    font="MarianneBold"
+                    className="  w-full   text-xl text-black"
+                  >
+                    {address.postcode} {address.city}
+                  </MyText>
+                </Pressable>
+              </View>
+            );
+          })}
+        </View>
+      </BottomSheetScrollView>
     </BottomSheet>
   );
 }
