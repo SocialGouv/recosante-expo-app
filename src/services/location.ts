dad890fe445aa2fe1702f1d3f1a3b5a8345914b9
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2024-01-28 21:12:21 +0100

    fix: location when gps not enabled

--- src/services/location.ts
+++ src/services/location.ts
@@ -1,25 +1,85 @@
 import * as Location from 'expo-location';
 import { Alert } from 'react-native';
 import { type Address, type Property } from '~/types/location';
+import { capture } from './sentry';
+
+type LocationRequestResponse = {
+  status: Location.PermissionStatus;
+  location: Location.LocationObject | null;
+  from?: string;
+};
 
 export namespace LocationService {
-  export async function requestLocation(): Promise<{
-    status: Location.PermissionStatus;
-    location: Location.LocationObject | null;
-  }> {
-    const { status } = await Location.requestForegroundPermissionsAsync();
-    if (status !== 'granted') {
-      return {
-        status,
-        location: null,
-      };
+  export async function requestLocation(): Promise<LocationRequestResponse> {
+    try {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      console.log('status', status);
+      if (status !== 'granted') {
+        return {
+          status,
+          location: null,
+        };
+      }
+      const requestLocationPromise: Promise<LocationRequestResponse> =
+        new Promise(async (resolve, reject) => {
+          try {
+            console.log('requestLocationPromise');
+            const gpsEnabled = await Location.hasServicesEnabledAsync();
+            console.log('gpsEnabled', gpsEnabled);
+            if (!gpsEnabled) {
+              console.log('!gpsEnabled');
+              resolve({
+                status: Location.PermissionStatus.DENIED,
+                location: null,
+                from: 'gps',
+              });
+            }
+
+            const lastKnownPosition =
+              await Location.getLastKnownPositionAsync();
+            console.log('lastKnownPosition', lastKnownPosition);
+            if (lastKnownPosition) {
+              console.log('lastKnownPosition');
+              resolve({ status, location: lastKnownPosition, from: 'cache' });
+            } else {
+              console.log('else');
+              const currentPosition = await Location.getCurrentPositionAsync({
+                accuracy: Location.Accuracy.Balanced,
+              });
+              resolve({
+                status,
+                location: currentPosition,
+                from: 'currentPosition',
+              });
+            }
+          } catch (error) {
+            reject(error);
+          }
+        });
+
+      const timerPromise = new Promise<LocationRequestResponse>((resolve) =>
+        setTimeout(() => {
+          console.log('timerPromise');
+          resolve({
+            status: Location.PermissionStatus.UNDETERMINED,
+            location: null,
+            from: 'timer',
+          });
+        }, 6000),
+      );
+
+      console.log('bim');
+      const faster = await Promise.race([requestLocationPromise, timerPromise]);
+      console.log('faster', faster);
+      return faster;
+    } catch (error) {
+      capture(error);
     }
-    const lastKnownPosition = await Location.getLastKnownPositionAsync();
-    if (lastKnownPosition) return { status, location: lastKnownPosition };
-    const currentPosition = await Location.getCurrentPositionAsync({
-      accuracy: Location.Accuracy.Balanced,
-    });
-    return { status, location: currentPosition };
+    console.log('return');
+    return {
+      status: Location.PermissionStatus.UNDETERMINED,
+      location: null,
+    };
   }
 
   export function formatPropertyToAddress(property: Property): Address {
