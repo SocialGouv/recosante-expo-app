a2e6cfdeba974f1b4001e17258a826b2b07f4ef1
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2024-01-29 17:25:12 +0100

    chore: app.json not well paramtered

---
+++ app.config.ts
@@ -0,0 +1,30 @@
+import { ExpoConfig, ConfigContext } from 'expo/config';
+
+// FIXME: not working, env variables not loading
+export default ({ config }: ConfigContext): ExpoConfig => {
+  return {
+    ...config,
+    name: 'Recosanté',
+    slug: 'recosante',
+    // android: {
+    //   googleServicesFile:
+    //     process.env.EAS_BUILD === 'local'
+    //       ? './google-services.json'
+    //       : process.env.GOOGLE_SERVICES_JSON,
+    //   ...config.android,
+    // },
+    // hooks: {
+    //   postPublish: [
+    //     {
+    //       file: 'sentry-expo/upload-sourcemaps',
+    //       config: {
+    //         organization: 'incubateur',
+    //         project: 'recosante-expo-app',
+    //         url: 'https://sentry.fabrique.social.gouv.fr/',
+    //         authToken: process.env.SENTRY_AUTH_TOKEN,
+    //       },
+    //     },
+    //   ],
+    // },
+  };
+};
