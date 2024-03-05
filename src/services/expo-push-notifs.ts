f655a16cfe29a28fa92e4cf35a8b84a156cc408c
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2024-03-05 15:29:25 +0100

    fix: push notifs

--- src/services/expo-push-notifs.ts
+++ src/services/expo-push-notifs.ts
@@ -1,6 +1,7 @@
 import { Platform, Alert, Linking } from 'react-native';
 import * as Notifications from 'expo-notifications';
 import Constants from 'expo-constants';
+import { capture } from './sentry';
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -35,51 +36,57 @@ export async function registerForPushNotificationsAsync({
   force = false,
   expo = false,
 } = {}) {
-  // if (Device.isDevice) {
-  const { status: existingStatus } = await Notifications.getPermissionsAsync();
-  let finalStatus = existingStatus;
-  if (existingStatus !== 'granted') {
-    if (!force) {
-      return null;
+  try {
+    // if (Device.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      if (!force) {
+        return null;
+      }
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
     }
-    const { status } = await Notifications.requestPermissionsAsync();
-    finalStatus = status;
-  }
-  if (finalStatus !== 'granted') {
-    Alert.alert(
-      'Les notifications sont désactivées',
-      'Vous pouvez activer les notifications dans les paramètres de votre téléphone.',
-      [
-        {
-          text: 'Open Settings',
-          onPress: async () => {
-            await Linking.openSettings();
+    if (finalStatus !== 'granted') {
+      Alert.alert(
+        'Les notifications sont désactivées',
+        'Vous pouvez activer les notifications dans les paramètres de votre téléphone.',
+        [
+          {
+            text: 'Open Settings',
+            onPress: async () => {
+              await Linking.openSettings();
+            },
           },
-        },
-        { text: 'OK', style: 'cancel', onPress: () => {} },
-      ],
-    );
+          { text: 'OK', style: 'cancel', onPress: () => {} },
+        ],
+      );
+      return;
+    }
+    const token = expo
+      ? await Notifications.getExpoPushTokenAsync({
+          // https://docs.expo.dev/versions/latest/sdk/notifications/#expopushtokenoptions
+          // "[...] it is recommended to set it manually."
+          projectId: '8d8b446e-c8db-4641-b730-5cef195e96da',
+          // projectId: Constants.expoConfig?.extra?.eas.projectId,
+        })
+      : await Notifications.getDevicePushTokenAsync();
+    // } else {
+    //   // alert("Must use physical device for Push Notifications");
+    // }
+
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+    return token;
+  } catch (e) {
+    capture(e);
     return;
   }
-  const token = expo
-    ? await Notifications.getExpoPushTokenAsync({
-        // https://docs.expo.dev/versions/latest/sdk/notifications/#expopushtokenoptions
-        // "[...] it is recommended to set it manually."
-        projectId: '8d8b446e-c8db-4641-b730-5cef195e96da',
-        // projectId: Constants.expoConfig?.extra?.eas.projectId,
-      })
-    : await Notifications.getDevicePushTokenAsync();
-  // } else {
-  //   // alert("Must use physical device for Push Notifications");
-  // }
-
-  if (Platform.OS === 'android') {
-    Notifications.setNotificationChannelAsync('default', {
-      name: 'default',
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: '#FF231F7C',
-    });
-  }
-  return token;
 }
