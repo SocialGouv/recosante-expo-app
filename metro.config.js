3cbd4f185db36ba3855339f1a5d621b9d226d267
charlesdoiron <doironcharles@gmail.com>
2024-03-11 20:50:33 +0100

    chore: update sentry

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,6 @@
-// This replaces `const { getDefaultConfig } = require('expo/metro-config');`
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { getSentryExpoConfig } = require('@sentry/react-native/metro');
 
-// This replaces `const config = getDefaultConfig(__dirname);`
 const config = getSentryExpoConfig(__dirname);
 
 module.exports = config;
