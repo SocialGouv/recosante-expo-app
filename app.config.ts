44d723d1b4a7a6ce5bad46b4e9d9dede8d7536fb
charlesdoiron <doironcharles@gmail.com>
2024-03-25 12:44:28 +0100

    chore: import dotenv in app.config.ts to access env during build

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,5 @@
+// Without dotenv, you can access .env
+import 'dotenv/config';
 import { type ExpoConfig, type ConfigContext } from 'expo/config';
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
@@ -7,11 +9,15 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   updates: {
     url: process.env.EXPO_PUBLIC_EXPO_UPDATES,
   },
+
   extra: {
     SENTRY_AUTH_TOKEN: process.env.SENTRY_AUTH_TOKEN,
     EXPO_PUBLIC_API_SCHEME: process.env.EXPO_PUBLIC_API_SCHEME,
     EXPO_PUBLIC_API_HOST: process.env.EXPO_PUBLIC_API_HOST,
     EXPO_PUBLIC_APP_ENV: process.env.EXPO_PUBLIC_APP_ENV,
+    eas: {
+      projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+    },
   },
 
   runtimeVersion: {
